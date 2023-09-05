d2d486f6940a9c003bfe73714309a4fdfa95baad
apav-dev <apavlick@yext.com>
2023-09-05 09:34:39 -0400

    copy template contents from starter repo: https://github.com/YextSolutions/pages-starter-blank

---
+++ src/templates/static.tsx
@@ -0,0 +1,102 @@
+/**
+ * This is an example of how to create a static template that uses getStaticProps to retrieve data.
+ */
+import * as React from "react";
+import { fetch } from "@yext/pages/util";
+import "../index.css";
+import {
+  Template,
+  GetPath,
+  GetHeadConfig,
+  HeadConfig,
+  TransformProps,
+  TemplateConfig,
+  TemplateProps,
+  TemplateRenderProps,
+} from "@yext/pages";
+import Favicon from "../public/yext-favicon.ico";
+
+/**
+ * Not required for static templates, but will contain the stream configuration for
+ * entity-powered templates.
+ */
+export const config: TemplateConfig = {
+  // The name of the feature. If not set the name of this file will be used (without extension).
+  // Use this when you need to override the feature name.
+  name: "static-example",
+};
+
+/**
+ * Used to either alter or augment the props passed into the template at render time.
+ * This function will be run during generation and pass in directly as props to the default
+ * exported function.
+ *
+ * This can be used when data needs to be retrieved from an external (non-Knowledge Graph)
+ * source. 
+ *
+ * If the page is truly static this function is not necessary.
+ */
+export const transformProps: TransformProps<TemplateRenderProps> = async (
+  data
+) => {
+  return data
+};
+
+/**
+ * Defines the path that the generated file will live at for production.
+ */
+export const getPath: GetPath<TemplateRenderProps> = () => {
+  return `index.html`;
+};
+
+
+/**
+ * This allows the user to define a function which will take in their template
+ * data and produce a HeadConfig object. When the site is generated, the HeadConfig
+ * will be used to generate the inner contents of the HTML document's <head> tag.
+ * This can include the title, meta tags, script tags, etc.
+ */
+export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
+  relativePrefixToRoot,
+  path,
+  document,
+}): HeadConfig => {
+  return {
+    title: "Static Page Example",
+    charset: "UTF-8",
+    viewport: "width=device-width, initial-scale=1",
+    tags: [
+      {
+        type: "meta",
+        attributes: {
+          name: "description",
+          content: "Static page example meta description.",
+        },
+      }
+    ],
+  };
+};
+
+
+/**
+ * This is the main template. It can have any name as long as it's the default export.
+ * The props passed in here are the direct result from `transformProps`.
+ */
+const Static: Template<TemplateRenderProps> = ({
+  relativePrefixToRoot,
+  path,
+  document,
+}) => {
+
+  // This is the site object from the Knowledge Graph. It contains all the data 
+  // for the site entity, and can be accessed in any template, including static templates. 
+  const { _site } = document;
+
+  return (
+    <>
+      <h1>Static Page</h1>
+    </>
+  );
+};
+
+export default Static;
